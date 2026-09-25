54d26190400776bbd62e390e441425c5c849c2e1
sleepycivet <318716591+sleepycivet@users.noreply.github.com>
2026-09-25 07:23:14 +0200

    Add text file for game text

---
+++ lib/hangman/text.rb
@@ -0,0 +1,16 @@
+module Text
+  def text_welcome
+    puts "Welcome to Hangman!"
+    puts "You will be guessing a random word, letter by letter."
+    # puts "Would you like start a new game or load a saved game?"
+  end
+
+  def text_prompt_new_or_load_game
+    puts "If you would like to start a new game, type 'new game'."
+    puts "If you would like to load a game, type 'load game'."
+  end
+
+  def text_game_start(correct_arr)
+    puts "word: " + correct_arr.join(" ")
+  end
+end
