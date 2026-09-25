7feccddad62b8ac318a58606abc26c816a947da8
sleepycivet <318716591+sleepycivet@users.noreply.github.com>
2026-09-25 09:36:41 +0200

    Change text_prompt_guess for more general use
    It used to just be an error message but I thought
    that it was a good beginning prompt too so I
    renamed it for general use.

--- lib/hangman/prompt.rb
+++ lib/hangman/prompt.rb
@@ -4,17 +4,18 @@ module Prompt
   include Text
 
   def prompt_guess
+    text_prompt_guess
     guess = nil
     while guess == nil do
       begin
         input = gets.downcase.to_s.chomp
       rescue
-        text_prompt_guess_error
+        text_prompt_guess
       else
         if input.match?(/[a-z]/) && input.length == 1
           guess = input
         else
-          text_prompt_guess_error
+          text_prompt_guess
         end
       end
     end
--- lib/hangman/text.rb
+++ lib/hangman/text.rb
@@ -18,7 +18,7 @@ module Text
     puts "incorrect: " + incorrect_arr.join(", ")
   end
   
-  def text_prompt_guess_error
+  def text_prompt_guess
     puts 'Please type a letter from a to z.'
   end
 end
